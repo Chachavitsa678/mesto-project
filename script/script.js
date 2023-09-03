e2273129006ed3c4e164ebdbb2cc21ccdbfb5edb
Полина Илянкина <polinailankina@MacBook-Pro-Polina.local>
2023-09-03 22:34:57 +0500

    добавлен npm, webpack, localhost, gitignore

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+import initialCards from "./new.js";
 // common consts 
 const elementsContainer = document.querySelector('.elements');
 const profileTitle = document.querySelector('.profile__title');
@@ -273,30 +274,5 @@ function handleDeleteButtonClick(event) {
 }
 
 
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
+
 
